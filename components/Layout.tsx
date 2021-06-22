78eb1b3ea64f7e058bbe147f4baa29756c5a10ec
Hanjeong Lee <gkswjdzz@naver.com>
2021-06-22 23:56:16 +0900

    Layout: add horizontal margin of content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,21 @@
+import { ReactElement } from 'react';
+
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
+import styles from '@/styles/Layout.module.scss';
+
 interface LayoutProps {
-  children: React.ReactElement;
+  children: ReactElement;
 }
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
-      {children}
+      <main className={styles.main}>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
