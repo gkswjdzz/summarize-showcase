939dd98de6eb23eef9cc6467502090ba6e9a1b66
Hanjeong Lee <gkswjdzz@naver.com>
2021-06-23 00:16:13 +0900

    Content: add Summarize Button

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,14 +1,25 @@
+import Image from 'next/image';
 import Share from '@/components/Share';
+import styles from '@/styles/Content.module.scss';
 
 const Content = () => {
   return (
-    <>
+    <div className={styles.content}>
       <div>
         <div>1</div>
         <div>2</div>
+        <button className={styles.button}>
+          <p>Summarize</p>
+          <Image
+            src='/icons/right-arrow.svg'
+            alt='right-arrow'
+            width={19}
+            height={14}
+          />
+        </button>
       </div>
       <Share />
-    </>
+    </div>
   );
 };
 
