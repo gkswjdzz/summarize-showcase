5c5fd414e742c2b98867d3b5559b07503529e76e
Hanjeong Lee <gkswjdzz@naver.com>
2021-06-22 23:48:52 +0900

    Header: update icon url

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <div className={cx('header')}>
       <div className={cx('logoAndTitles')}>
-        <Image src='/logo.svg' width={57} height={57} />
+        <Image src='/icons/logo.svg' alt='logo' width={57} height={57} />
         <div className={cx('titles')}>
           <h1 className={cx('title')}>Summar Summar</h1>
           <p className={cx('subtitle')}>
@@ -20,8 +20,12 @@ const Header = () => {
       </div>
 
       <div className={cx('links')}>
-        <Button type='medium' text='GitHub' src='/github.svg' />
-        <Button type='medium' text='Testing API' src='/ainize.svg' />
+        <Button type='medium' text='GitHub' src='/icons/github-white.svg' />
+        <Button
+          type='medium'
+          text='Testing API'
+          src='/icons/ainize-white.svg'
+        />
       </div>
     </div>
   );
