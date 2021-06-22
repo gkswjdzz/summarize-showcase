af896d0351b0de547eb84a4e30c5ef9f69cea881
Hanjeong Lee <gkswjdzz@naver.com>
2021-06-23 00:15:48 +0900

    Footer: add Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,20 @@
+import Image from 'next/image';
+
+import styles from '@/styles/Footer.module.scss';
+
 const Footer = () => {
-  return <></>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.link}>
+        <Image src='/icons/ainize.svg' alt='ainize' width={14} height={14} />
+        <p>Powered by Ainize</p>
+      </div>
+      <div className={styles.link}>
+        <Image src='/icons/github.svg' alt='github' width={17} height={17} />
+        <p>Contribute on GitHub</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
